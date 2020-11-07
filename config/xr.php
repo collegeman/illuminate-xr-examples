d861cd3e8dffd578f37fdab990e03cf053335a74
Aaron Collegeman <aaron@c21redwood.com>
2020-11-07 09:49:03 -0500

    init

---
+++ config/xr.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Examples
+    |--------------------------------------------------------------------------
+    |
+    | The XR package ships with examples that will help you to better leverage
+    | the package in your own projects. By default, these examples are disabled.
+    | You can enable the examples by setting the "examples" setting below to
+    | true. You'll then be able to browse the examples in your app at
+    | /xr/examples.
+    |
+    */
+
+    'examples' => true, // Set to true to enable the example experiences
+
+];
